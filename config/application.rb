ef6113fe6ca3e657c3e0e94aafc78320204d1cf2
GarPit <garik.piton@gmail.com>
2014-04-28 20:47:41 +0400

    add secret token

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module EmailSubscribe
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
+    #config.secret_key_base = YAML.load(File.open("#{Rails.root}/config/secrets.yml"))[Rails.env]['secret_key_base']
 
     config.action_mailer.smtp_settings = {
       :address        => 'smtp.gmail.com',
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmailSubscribe::Application.config.secret_key_base = '375841613fae35ca88ba9d651e94397d311894ae0b02405cc57424723cb3a44224fd7fdd72029ca5145a9249fdab1cc67d7f3f2c44cc47fbdbbf29ee3856f8fd'
